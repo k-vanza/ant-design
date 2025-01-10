dc0c105281727b3c034315d6bfdaeccba562f7de
kanishk.v <kanishk.v@yanoljacloudsolution.com>
2025-01-10 13:30:17 +0530

    icons

---
+++ src/tutorials/TutorialEleven.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {SettingOutlined} from '@ant-design/icons'
+
+const TutorialEleven = () => {
+  return (
+    <>
+        <SettingOutlined
+            spin={true}
+            style={{fontSize: '50px'}}
+        />
+    </>
+  )
+}
+
+export default TutorialEleven
